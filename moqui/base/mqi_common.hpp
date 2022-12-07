bd826e06c51b3f674215d482d620d4a8a7f38bb8
ferdymercury <ferdymercury@users.noreply.github.com>
2022-12-07 14:57:04 +0100

    Backport atomicAdd function to older CUDA architectures

--- moqui/base/mqi_common.hpp
+++ moqui/base/mqi_common.hpp
@@ -97,4 +97,25 @@ const float max_step_global = 1.0;
 
 }   // namespace mqi
 
+#if defined(__CUDACC__)
+#include <cuda.h>
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600
+/**
+ * \brief atomic add function for older CUDA architectures
+ * \see https://stackoverflow.com/questions/39274472/error-function-atomicadddouble-double-has-already-been-defined
+ */
+static __inline__ __device__ double atomicAdd(double *address, double val) {
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    if (val==0.0)
+        return __longlong_as_double(old);
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val +__longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+#endif
+#endif
+
 #endif
